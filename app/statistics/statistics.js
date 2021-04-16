68cc1e718458498c4a6ef575343b643cee4c1e44
bwadaal <v.i.boisdon@gmail.com>
2021-04-16 21:54:50 +0200

    added the logo and adapted the statistics for better phone usage and better graph titles

--- app/statistics/statistics.js
+++ app/statistics/statistics.js
@@ -34,7 +34,6 @@ angular.module('myApp.statistics', ['ngRoute'])
             $scope.loadGraphs = function () {
                 for (let [i, graph] of $scope.graphs.entries()) {
                     let chartCtx = document.getElementById(`chart-${i}`);
-                    console.log(chartCtx);
                     let chart = new Chart(chartCtx, {
                         type: 'line',
                         data: graph.data,
@@ -43,7 +42,7 @@ angular.module('myApp.statistics', ['ngRoute'])
                             maintainAspectRatio: false,
                             title: {
                                 text: graph.title,
-                                display: true
+                                display: false
                             },
                             scales: {
                                 yAxes: [{
@@ -51,6 +50,55 @@ angular.module('myApp.statistics', ['ngRoute'])
                                         beginAtZero: true
                                     }
                                 }]
+                            },
+                            legend: {
+                                onClick: function(e, legendItem) {
+                                    var index = legendItem.datasetIndex;
+                                    var ci = this.chart;
+                                    var alreadyHidden = (ci.getDatasetMeta(index).hidden === null) ? false : ci.getDatasetMeta(index).hidden;
+                                    var anyOthersAlreadyHidden = false;
+                                    var allOthersHidden = true;
+
+                                    // figure out the current state of the labels
+                                    ci.data.datasets.forEach(function(e, i) {
+                                        var meta = ci.getDatasetMeta(i);
+
+                                        if (i !== index) {
+                                            if (meta.hidden) {
+                                                anyOthersAlreadyHidden = true;
+                                            } else {
+                                                allOthersHidden = false;
+                                            }
+                                        }
+                                    });
+
+                                    // if the label we clicked is already hidden
+                                    // then we now want to unhide (with any others already unhidden)
+                                    if (alreadyHidden) {
+                                        ci.getDatasetMeta(index).hidden = null;
+                                    } else {
+                                        // otherwise, lets figure out how to toggle visibility based upon the current state
+                                        ci.data.datasets.forEach(function(e, i) {
+                                            var meta = ci.getDatasetMeta(i);
+
+                                            if (allOthersHidden) {
+                                                meta.hidden = null;
+                                            } else {
+                                                if (anyOthersAlreadyHidden) {
+                                                    if (i === index) {
+                                                        meta.hidden = true;
+                                                    }
+                                                } else {
+                                                    if (i !== index) {
+                                                        meta.hidden = true;
+                                                    }
+                                                }
+                                            }
+                                        });
+                                    }
+
+                                    ci.update();
+                                }
                             }
                         }
                     });
--- back/server.js
+++ back/server.js
@@ -66,22 +66,28 @@ function fetchGraphData(lea) {
                 if (err) {
                     return console.log(err)
                 }
-                if (body.events) {
+                if (body?.events) {
                     for (let match of body.events) {
                         for (let [i, team] of final.entries()) { //on itère sur nos équipes pour trouver les 2 concernées
-                            if (!match.intHomeScore) {
+                            if (!match.intHomeScore || !match.intAwayScore) {
                                 break;
                             }
                             if (team.id === match.idHomeTeam) {
-                                final[i].team[counter[team.id].played + 1].goalsfor = parseInt(match.intHomeScore) + final[i].team[counter[team.id].played].goalsfor;
-                                final[i].team[counter[team.id].played + 1].goalsagainst = parseInt(match.intAwayScore) + final[i].team[counter[team.id].played].goalsagainst;
-                                final[i].team[counter[team.id].played + 1].goalsdiff += parseInt(match.intHomeScore) - parseInt(match.intAwayScore) + final[i].team[counter[team.id].played].goalsdiff;
+                                final[i].team[counter[team.id].played + 1].sumFor = parseInt(match.intHomeScore) + final[i].team[counter[team.id].played].sumFor;
+                                final[i].team[counter[team.id].played + 1].sumAgainst = parseInt(match.intAwayScore) + final[i].team[counter[team.id].played].sumAgainst;
+                                final[i].team[counter[team.id].played + 1].sumDiff += parseInt(match.intHomeScore) - parseInt(match.intAwayScore) + final[i].team[counter[team.id].played].sumDiff;
+                                final[i].team[counter[team.id].played + 1].for = parseInt(match.intHomeScore);
+                                final[i].team[counter[team.id].played + 1].against = parseInt(match.intAwayScore);
+                                final[i].team[counter[team.id].played + 1].diff += parseInt(match.intHomeScore) - parseInt(match.intAwayScore)
                                 counter[team.id].played += 1;
                             }
                             if (team.id === match.idAwayTeam) {
-                                final[i].team[counter[team.id].played + 1].goalsfor = parseInt(match.intAwayScore) + final[i].team[counter[team.id].played].goalsfor;
-                                final[i].team[counter[team.id].played + 1].goalsagainst = parseInt(match.intHomeScore) + final[i].team[counter[team.id].played].goalsagainst;
-                                final[i].team[counter[team.id].played + 1].goalsdiff += parseInt(match.intAwayScore) - parseInt(match.intHomeScore) + final[i].team[counter[team.id].played].goalsdiff;
+                                final[i].team[counter[team.id].played + 1].sumFor = parseInt(match.intAwayScore) + final[i].team[counter[team.id].played].sumFor;
+                                final[i].team[counter[team.id].played + 1].sumAgainst = parseInt(match.intHomeScore) + final[i].team[counter[team.id].played].sumAgainst;
+                                final[i].team[counter[team.id].played + 1].sumDiff += parseInt(match.intAwayScore) - parseInt(match.intHomeScore) + final[i].team[counter[team.id].played].sumDiff;
+                                final[i].team[counter[team.id].played + 1].for = parseInt(match.intAwayScore);
+                                final[i].team[counter[team.id].played + 1].against = parseInt(match.intHomeScore);
+                                final[i].team[counter[team.id].played + 1].diff += parseInt(match.intAwayScore) - parseInt(match.intHomeScore);
                                 counter[team.id].played += 1;
                             }
                         }
@@ -96,77 +102,87 @@ function fetchGraphData(lea) {
 }
 
 function createGraphData(lea, final, lastRound, counter) {
-    var diffData = {};
-    var forData = {};
-    var againstData = {};
-    var labels = [];
+    let diffData = {};
+    let forData = {};
+    let againstData = {};
+    let sumDiffData = {};
+    let sumForData = {};
+    let sumAgainstData = {};
+    let labels = [];
     for (var i=0; i<=lastRound; i++) {
         labels.push('Round '+i);
     }
-    diffData.labels = labels;
-    forData.labels = labels;
-    againstData.labels = labels;
-    diffData.datasets=[];
-    forData.datasets=[];
-    againstData.datasets=[];
+    for (let data of [diffData, forData, againstData, sumDiffData, sumForData, sumAgainstData]) {
+        data.labels = labels;
+        data.datasets = [];
+    }
     let teamsFetched = 0;
     for (let team of final) {
         let diffDataset = {};
         let forDataset = {};
         let againstDataset = {};
+        let sumDiffDataset = {};
+        let sumForDataset = {};
+        let sumAgainstDataset = {};
         request(`https://thesportsdb.com/api/v1/json/${process.env.API_KEY}/lookupteam.php?id=${team.id}`, {json: true}, (err, res, body) => {
             if (err) {return console.log(err)}
             var club = body.teams[0];
             let color = getRandomColor();
-            diffDataset.label = club.strTeam;
-            diffDataset.data = [];
-            diffDataset.tension = 0.2;
-            diffDataset.cubicInterpolationMode = 'default';
-            diffDataset.borderColor = color;
-            diffDataset.backgroundColor = 'rgba(255,255,255,0)';
-            diffDataset.borderWidth = 1;
-            forDataset.label = club.strTeam;
-            forDataset.data = [];
-            forDataset.tension = 0.2;
-            forDataset.cubicInterpolationMode = 'default';
-            forDataset.borderColor = color;
-            forDataset.backgroundColor = 'rgba(255,255,255,0)';
-            forDataset.borderWidth = 1;
-            againstDataset.label = club.strTeam;
-            againstDataset.data = [];
-            againstDataset.tension = 0.2;
-            againstDataset.cubicInterpolationMode = 'default';
-            againstDataset.borderColor = color;
-            againstDataset.backgroundColor = 'rgba(255,255,255,0)';
-            againstDataset.borderWidth = 1;
+            for (let dataset of [diffDataset, forDataset, againstDataset, sumDiffDataset, sumForDataset, sumAgainstDataset]) {
+                dataset.label = club.strTeam;
+                dataset.data = [];
+                dataset.tension = 0.2;
+                dataset.cubicInterpolationMode = 'default';
+                dataset.borderColor = color;
+                dataset.backgroundColor = 'rgba(255,255,255,0)';
+                dataset.borderWidth = 1;
+            }
             for (let i=0; i<=counter[team.id].played; i++) {
                 let round = team.team[i];
-                diffDataset.data.push(round.goalsdiff);
-                forDataset.data.push(round.goalsfor);
-                againstDataset.data.push(round.goalsagainst);
+                sumDiffDataset.data.push(round.sumDiff);
+                sumForDataset.data.push(round.sumFor);
+                sumAgainstDataset.data.push(round.sumAgainst);
+                diffDataset.data.push(round.diff);
+                forDataset.data.push(round.for);
+                againstDataset.data.push(round.against);
             }
             diffData.datasets.push(diffDataset);
             forData.datasets.push(forDataset);
             againstData.datasets.push(againstDataset);
+            sumDiffData.datasets.push(sumDiffDataset);
+            sumForData.datasets.push(sumForDataset);
+            sumAgainstData.datasets.push(sumAgainstDataset);
             teamsFetched ++;
             if (teamsFetched === final.length)
-                saveGraphData(lea, diffData, forData, againstData);
+                saveGraphData(lea, diffData, forData, againstData, sumDiffData, sumForData, sumAgainstData);
         });
     }
 }
 
-async function saveGraphData(lea, diffData, forData, againstData) {
-    await Data.deleteOne({title: `Cumulative point difference ${lea.name}`});
-    let diff = new Data({title: `Cumulative point difference ${lea.name}`, data: diffData, league_id: lea.id});
+async function saveGraphData(lea, diffData, forData, againstData, sumDiffData, sumForData, sumAgainstData) {
+    await Data.deleteOne({title: `Score gap for each match`, league_id: lea.id});
+    let diff = new Data({title: `Score gap for each match`, data: diffData, league_id: lea.id});
     diff.save().then(() => {});
 
-    await Data.deleteOne({title: `Cumulative points scored ${lea.name}`});
-    let fors = new Data({title: `Cumulative points scored ${lea.name}`, data: forData, league_id: lea.id});
+    await Data.deleteOne({title: `Points scored for each match`, league_id: lea.id});
+    let fors = new Data({title: `Points scored for each match`, data: forData, league_id: lea.id});
     fors.save().then(() => {});
 
-    await Data.deleteOne({title: `Cumulative points taken ${lea.name}`});
-    let against = new Data({title: `Cumulative points taken ${lea.name}`, data: againstData, league_id: lea.id});
+    await Data.deleteOne({title: `Points taken for each match`, league_id: lea.id});
+    let against = new Data({title: `Points taken for each match`, data: againstData, league_id: lea.id});
     against.save().then(() => {});
+
+    await Data.deleteOne({title: `Sum of the score gaps`, league_id: lea.id});
+    let sumDiff = new Data({title: `Sum of the score gaps`, data: sumDiffData, league_id: lea.id});
+    sumDiff.save().then(() => {});
+
+    await Data.deleteOne({title: `Total points scored`, league_id: lea.id});
+    let sumFors = new Data({title: `Total points scored`, data: sumForData, league_id: lea.id});
+    sumFors.save().then(() => {});
+
+    await Data.deleteOne({title: `Total points taken`, league_id: lea.id});
+    let sumAgainst = new Data({title: `Total points taken`, data: sumAgainstData, league_id: lea.id});
+    sumAgainst.save().then(() => {});
 }
 
 function initFinal(counter, rounds){
@@ -174,7 +190,7 @@ function initFinal(counter, rounds){
     for (let team of Object.entries(counter)) {     //puis pour chaque équipe qui a été détectée grâce à la 1ere requête
         var teamf = [];    //On enregistre un tableau pour contenir les évolutions de score de l'équipe au corus de la saison
         for (var round=0; round<=rounds; round++) {
-            teamf.push({'round': round, 'goalsfor': 0, 'goalsagainst': 0, 'goalsdiff': 0});
+            teamf.push({'round': round, 'sumFor': 0, 'sumAgainst': 0, 'sumDiff': 0, 'for': 0, 'against': 0, 'diff': 0});
         }
         final.push({'team':teamf, 'id': team[0]});
     }
